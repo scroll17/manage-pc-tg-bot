fca23ab8221206526ef9f19a59ba6bfb87720ed5
scroll17 <47834834+scroll17@users.noreply.github.com>
2019-11-22 21:42:11 +0200

    update "exit"-process

--- config/process.js
+++ config/process.js
@@ -1,1 +1,5 @@
 process.env.BOT_TOKEN = "1009977345:AAGgtj8tTao_hnFGGvs3OUK2EDPWuagviUI";
+
+// 0 - 'success' code 
+// 130 - Script terminated by Control-C
+process.exitCode = 130;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { os }  = require('./config');
-
 const { computerIsOn } = require('./actions')(os);
+
+const state = require('./state/state');
 
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -19,8 +20,17 @@ bot.start((ctx) => {
 })
 
 bot.command(EXIT, (ctx) => {
-    ctx.reply('Do you exit ?');
-    process.exit(0);
+    state.set('exit', process.exitCode);
+    ctx.reply('Введите что-то для завершения роботы.');
 });
+
+bot.use((ctx) => {
+    if(state.has('exit')){
+        ctx.reply('Bot closing...');
+
+        const exitCode = state.get('exit');
+        process.exit(exitCode);
+    }
+})
 
 bot.launch()
