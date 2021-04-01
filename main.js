21184c82cc1c86ac74bde3aa71a3b05726040af6
Денис <zololotarenko.2015@gmail.com>
2021-04-01 15:42:48 +0300

    some update

---
+++ main.js
@@ -0,0 +1,52 @@
+const { os }  = require('./config');
+const { computerIsOn } = require('./actions')(os);
+const address = require('address');
+const internalIp = require('internal-ip');
+
+const fs = require('fs');
+const path = require('path');
+
+
+
+
+const state = require('./state/state');
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const {
+    commands: {
+        EXIT
+    }
+} = require('./contstants');
+
+
+bot.start(async (ctx) => {
+    let ip;
+    try {
+        ip = await internalIp.v4()
+    } catch {}
+
+    ctx.reply(`
+        local ip: ${address.ip()}
+        ip: ${ip}
+    `);
+})
+
+bot.command(EXIT, (ctx) => {
+    state.set('exit', process.exitCode);
+    ctx.reply('Введите что-то для завершения роботы.');
+});
+
+
+
+// bot.use((ctx) => {
+//     if(state.has('exit')){
+//         ctx.reply('Bot closing...');
+//
+//         const exitCode = state.get('exit');
+//         process.exit(exitCode);
+//     }
+// })
+
+bot.launch()
